5088bc93db46c0488efe0460506432281373a2a5
siddhansh bansal <bansalsid2000@gmail.com>
2024-03-08 22:56:45 +0530

    request in json format

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ connectMongoDB("mongodb+srv://bansalsid2000:eucZB1kvKz2YgoPb@cluster0.1q28hsg.mo
 
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
 
 app.use( (req,res,next)=>{
     console.log("hello from middleware");
